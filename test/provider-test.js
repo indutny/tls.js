443287f730c8173dbc3adc1ba9c6537335e487e0
Fedor Indutny <fedor@indutny.com>
2014-04-11 16:23:23 +0400

    provider: steps toward working handshake

---
+++ test/provider-test.js
@@ -0,0 +1,18 @@
+var tls = require('..'),
+    assert = require('assert');
+
+var PORT = 8000;
+
+describe('tls.js/Provider', function() {
+  var provider = tls.provider.node.create();
+
+  it('should create PRF', function() {
+    var prf = new provider.prf(
+      'sha256',
+      new Buffer('1234'),
+      new Buffer('label'),
+      new Buffer('seed'));
+
+    assert.equal(prf.read(256).length, 256);
+  });
+});
