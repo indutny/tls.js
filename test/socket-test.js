d6d15b912037394e6e9bea5e48702b66f4b674d5
Fedor Indutny <fedor@indutny.com>
2014-08-26 02:30:50 +0400

    lib: fix interaction with OpenSSL

--- test/socket-test.js
+++ test/socket-test.js
@@ -1,57 +1,81 @@
 var tls = require('..'),
+    ntls = require('tls'),
     net = require('net'),
     fs = require('fs'),
     assert = require('assert');
 
 var PORT = 8000;
 
+var certs = {
+  key: fs.readFileSync(__dirname + '/keys/key.pem'),
+  cert: fs.readFileSync(__dirname + '/keys/cert.pem')
+};
+
 describe('tls.js/Socket', function() {
-  var server;
-  var client;
-  var serverSide;
-  var clientSide;
+  describe('ping/pong', function() {
+    var server;
+    var client;
+    var serverSide;
+    var clientSide;
 
-  beforeEach(function(done) {
-    var waiting = 2;
-    function wait() {
-      if (--waiting === 0) done();
-    }
+    beforeEach(function(done) {
+      var waiting = 2;
+      function wait() {
+        if (--waiting === 0) done();
+      }
 
-    var provider = tls.provider.node.create();
-    var ctx = {
-      client: tls.context.create({ provider: provider }),
-      server: tls.context.create({ provider: provider })
-    };
-    ctx.server.addKeyPair('rsa', {
-      key: fs.readFileSync(__dirname + '/keys/key.pem'),
-      cert: fs.readFileSync(__dirname + '/keys/cert.pem')
+      var provider = tls.provider.node.create();
+      var ctx = {
+        client: tls.context.create({ provider: provider }),
+        server: tls.context.create({ provider: provider })
+      };
+      ctx.server.addKeyPair('rsa', certs);
+
+      server = net.createServer(function(socket) {
+        serverSide = tls.socket.create(socket, ctx.server, 'server');
+        serverSide.start();
+        wait();
+      }).listen(PORT, function() {
+        client = net.connect(PORT, function() {
+          clientSide = tls.socket.create(client, ctx.client, 'client');
+          clientSide.start();
+          wait();
+        });
+      });
     });
 
-    server = net.createServer(function(socket) {
-      serverSide = tls.socket.create(socket, ctx.server, 'server');
-      serverSide.state.on('stateChange', function(from, to) {
-        console.log('s %s >> %s', from, to);
-      });
-      serverSide.start();
-      wait();
-    }).listen(PORT, function() {
-      client = net.connect(PORT, function() {
-        clientSide = tls.socket.create(client, ctx.client, 'client');
-        clientSide.state.on('stateChange', function(from, to) {
-          console.log('c %s >> %s', from, to);
-        });
-        clientSide.start();
-        wait();
-      });
+    afterEach(function(done) {
+      client.destroy();
+      server.close(done);
+    });
+
+    it('should establish secure connection', function(done) {
+      var waiting = 2;
+      function wait() {
+        if (--waiting === 0) done();
+      }
+      serverSide.on('secure', wait);
+      clientSide.on('secure', wait);
     });
   });
 
-  it('should establish secure connection', function(done) {
-    var waiting = 2;
-    function wait() {
-      if (--waiting === 0) done();
-    }
-    serverSide.on('secure', wait);
-    clientSide.on('secure', wait);
+  describe('interacting with OpenSSL', function() {
+    var provider = tls.provider.node.create();
+    var ctx = tls.context.create({ provider: provider });
+
+    it('should connect to OpenSSL server', function(done) {
+      var client;
+      var server = ntls.createServer(certs, function(c) {
+        c.destroy();
+        server.close(done);
+      }).listen(PORT, function() {
+        client = net.connect(PORT, onConnect);
+      });
+
+      function onConnect() {
+        var s = tls.socket.create(client, ctx, 'client');
+        s.start();
+      }
+    });
   });
 });
