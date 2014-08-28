219fcb733ecd2c1f7532a2e38366c274c2090c1c
Fedor Indutny <fedor@indutny.com>
2014-08-29 00:59:22 +0400

    utils: move PRF to utils

--- test/provider-test.js
+++ test/provider-test.js
@@ -3,11 +3,11 @@ var tls = require('..'),
 
 var PORT = 8000;
 
-describe('tls.js/Provider', function() {
+describe('tls.js/Utils', function() {
   var provider = tls.provider.node.create();
 
   it('should create PRF', function() {
-    var prf = new provider.prf('sha256')(
+    var prf = new tls.utils.prf(provider.mac('sha256'))(
       new Buffer('1234'),
       new Buffer('label'),
       new Buffer('seed'));
