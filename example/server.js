898c945585599ce34913752ad63352b01602ecf9
Fedor Indutny <fedor@indutny.com>
2014-08-29 17:20:53 +0400

    parser: less allocations?

--- example/server.js
+++ example/server.js
@@ -5,19 +5,7 @@ var server = tls.createHTTPServer({
   key: fs.readFileSync(__dirname + '/../test/keys/key.pem'),
   cert: fs.readFileSync(__dirname + '/../test/keys/cert.pem'),
   ciphers: [
-    'TLS_RSA_WITH_AES_256_CBC_SHA',
-    'TLS_RSA_WITH_AES_128_CBC_SHA',
-    'TLS_RSA_WITH_AES_256_CBC_SHA256',
-    'TLS_RSA_WITH_AES_128_CBC_SHA256',
-    'TLS_RSA_WITH_DES_CBC_SHA',
-    'TLS_RSA_WITH_3DES_EDE_CBC_SHA',
-    'TLS_RSA_WITH_RC4_128_MD5',
-    'TLS_RSA_WITH_RC4_128_SHA',
-    'TLS_RSA_WITH_NULL_SHA',
-    'TLS_RSA_WITH_IDEA_CBC_SHA',
-    'TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA',
-    'TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256',
-    'TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384'
+    'TLS_RSA_WITH_AES_256_CBC_SHA'
   ],
 }, function(req, res) {
   res.end('Hello world!');
