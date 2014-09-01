16a573efe84a17db2d60a5e42c177d5f8a460144
Fedor Indutny <fedor@indutny.com>
2014-09-01 16:50:00 +0400

    state: use raw-rsa

--- example/server.js
+++ example/server.js
@@ -1,16 +1,31 @@
 var tls = require('../');
+var https = require('https');
 var fs = require('fs');
+var cluster = require('cluster');
 
-var server = tls.createHTTPServer({
+var node = https.createServer({
+  key: fs.readFileSync(__dirname + '/../test/keys/key.pem'),
+  cert: fs.readFileSync(__dirname + '/../test/keys/cert.pem'),
+  ciphers: 'AES256-SHA',
+}, onRequest);
+node.listen(1443, onListen.bind(node, 'node.js'));
+
+var tlsjs = tls.createHTTPServer({
   key: fs.readFileSync(__dirname + '/../test/keys/key.pem'),
   cert: fs.readFileSync(__dirname + '/../test/keys/cert.pem'),
   ciphers: [
     'TLS_RSA_WITH_AES_256_CBC_SHA'
   ],
-}, function(req, res) {
+}, onRequest);
+tlsjs.listen(1444, onListen.bind(tlsjs, 'tls.js'));
+
+function onRequest(req, res) {
   res.end('Hello world!');
-}).listen(1443, function() {
-  console.log('Listening on [%s]:%d',
+}
+
+function onListen(name) {
+  console.log('<%s> listening on [%s]:%d',
+              name,
               this.address().address,
               this.address().port);
-});
+}
